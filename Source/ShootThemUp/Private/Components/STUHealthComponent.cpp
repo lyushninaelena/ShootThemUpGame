573dc954acdc3c6509b8b63c9954d085c1b409c1
Elena Liushnina <lyushnina@gmail.com>
2021-08-29 20:16:25 +0300

    35 lesson. apply damage

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -4,6 +4,8 @@
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
+
 USTUHealthComponent::USTUHealthComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
@@ -27,4 +29,6 @@ void USTUHealthComponent::OnTakeAnyDamage(
 	AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
 	Health -= Damage;
+
+	UE_LOG(LogHealthComponent, Display, TEXT("Damage: %f"), Damage);
 }
--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -48,8 +48,6 @@ void ASTUBaseCharacter::Tick(float DeltaTime)
 
 	const float Health = HealthComponent->GetHealth();
 	HealthTextComponent->SetText(FText::FromString(FString::Printf(TEXT("% .0f"), Health)));
-
-	TakeDamage(0.1, FDamageEvent{}, Controller, this);
 }
 
 // Called to bind functionality to input
