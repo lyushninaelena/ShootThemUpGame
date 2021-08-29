a18ec0f0b62b5fbe6c68fbb8cd2e8249a2aeca39
Elena Liushnina <lyushnina@gmail.com>
2021-08-29 18:35:35 +0300

    33 lesson. health component

---
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -0,0 +1,16 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "Components/STUHealthComponent.h"
+
+USTUHealthComponent::USTUHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void USTUHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;	
+}
--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -8,6 +8,8 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class USTUHealthComponent;
+class UTextRenderComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
@@ -24,6 +26,12 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UCameraComponent* CameraComponent;	
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USTUHealthComponent* HealthComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UTextRenderComponent* HealthTextComponent;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
