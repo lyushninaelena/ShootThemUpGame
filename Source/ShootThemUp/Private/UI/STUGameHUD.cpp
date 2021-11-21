eecf6877ca91d541ce8ed84fb2b04cf562425d40
Elena Liushnina <lyushnina@gmail.com>
2021-11-22 00:22:22 +0300

    68 lesson. health progressbar cpp version

--- Source/ShootThemUp/Private/UI/STUGameHUD.cpp
+++ Source/ShootThemUp/Private/UI/STUGameHUD.cpp
@@ -1,6 +1,8 @@
 // Shoot Them Up Game. All Rights Reserved.
 
 #include "UI/STUGameHUD.h"
+
+#include "Blueprint/UserWidget.h"
 #include "Engine/Canvas.h"
 
 void ASTUGameHUD::DrawHUD()
@@ -8,6 +10,17 @@ void ASTUGameHUD::DrawHUD()
 	Super::DrawHUD();
 
 	DrawCrosshair();
+}
+
+void ASTUGameHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass);
+	if (PlayerHUDWidget)
+	{
+		PlayerHUDWidget->AddToViewport();
+	}
 }
 
 void ASTUGameHUD::DrawCrosshair()
---
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -0,0 +1,18 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "UI/STUPlayerHUDWidget.h"
+
+#include "STUHealthComponent.h"
+
+float USTUPlayerHUDWidget::GetHealthPercent() const
+{
+	const auto Player = GetOwningPlayerPawn();
+	if (!Player) return 0.0f;
+
+	const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
+	const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+	if (!HealthComponent) return 0.0f;
+
+	return HealthComponent->GetHealthPercent();
+}
