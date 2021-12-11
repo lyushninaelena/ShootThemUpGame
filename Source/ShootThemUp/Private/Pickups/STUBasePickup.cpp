088e8ee8b12d324e1d8882a91f64cfc72215eef1
Elena Liushnina <lyushnina@gmail.com>
2021-12-11 16:44:41 +0300

    78 lesson. pickup rotation

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -22,6 +22,7 @@ void ASTUBasePickup::BeginPlay()
 
 	check(CollisionComponent)
 	
+	GenerateRotationYaw();
 }
 
 void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -39,6 +40,7 @@ void ASTUBasePickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
@@ -60,9 +62,18 @@ void ASTUBasePickup::PickupWasTaken()
 
 void ASTUBasePickup::Respawn()
 {
+	GenerateRotationYaw();
+
 	if (GetRootComponent())
 	{
 		GetRootComponent()->SetVisibility(true, true);
 	}
+
 	CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+}
+
+void ASTUBasePickup::GenerateRotationYaw()
+{
+	const float Direction = FMath::RandBool() ? 1.0f : -1.0f;
+	RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
--- Source/ShootThemUp/Public/Pickups/STUBasePickup.h
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -33,8 +33,13 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
+	float RotationYaw = 0.0f;
+
 	virtual bool GivePickupTo(APawn* PlayerPawn);
+
 	void PickupWasTaken();
 	void Respawn();
+	void GenerateRotationYaw();
+
 
 };
